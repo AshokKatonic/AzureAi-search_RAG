8a77479ba2a7e453abaf95f266cf9124737a18b6
AshokKatonic <ashok.kumar@katonic.ai>
2025-09-26 19:01:25 +0530

    Update test.py

--- test.py
+++ test.py
@@ -8,9 +8,9 @@ load_dotenv()
 print(f"🔍 Environment variable: {os.getenv('AZURE_OPENAI_EMBEDDING_DEPLOYED_MODEL_NAME')}")
 
 openai_client = AzureOpenAI(
-    api_key="3lDUZwn4FGGz4UFp87YH24TgNOaY9M43f0vJHttVORNfWpkv9ziTJQQJ99BIACYeBjFXJ3w3AAABACOGdqM5",
+    api_key="3lDUZwn4FGGz4UFp8xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
     api_version="2024-02-01",
-    azure_endpoint="https://eshal-openai-services.openai.azure.com/"
+    azure_endpoint="https://xxxxx-openai-serxxxxxxxxxxxxxxx"
 )
 
 try:
@@ -20,4 +20,5 @@ try:
     )
     print("✅ Embedding model works!")
 except Exception as e:
+
     print(f"❌ Error: {e}")
